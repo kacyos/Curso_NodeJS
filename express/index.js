207681df0a46216d54f49506d279ffc57855ffe4
Cacio de Castro <cacio.castro@gmail.com>
2021-02-05 17:54:10 -0300

    alterações de aula

--- express/index.js
+++ express/index.js
@@ -1,6 +1,11 @@
 const express = require('express'); // Importa o express
 const app = express(); // Inicia o express
 
+app.get("/", function(request, response){
+    response.send("Bem vindo!")
+    response.send('De novo')
+})
+
 app.listen(4000, function (erro) {
    if (erro) {
        console.log('Ocorreu um erro ao iniciar o servidor! ')
