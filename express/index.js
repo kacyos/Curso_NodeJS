733cc21011702d0a3c1010f33361b5cddb1eabb7
Cacio de Castro <cacio.castro@gmail.com>
2021-02-06 12:27:28 -0300

    inclusão do readme

--- express/index.js
+++ express/index.js
@@ -1,15 +1,23 @@
 const express = require('express'); // Importa o express
 const app = express(); // Inicia o express
 
-app.get("/", function(request, response){
-    response.send("Bem vindo!")
-    response.send('De novo')
-})
+app.get('/', function (request, response) {
+  response.send('Bem vindo!');
+  //response.send('De novo'); não é permitido ter duas respostas
+});
+
+app.get('/blog', function (req, res) {
+  res.send('<h1>Bem vindo ao blog!</h1>');
+});
+
+app.get('/canal/youtube', function (req, res) {
+  res.send('<h1>Bem vindo ao meu canal!</h1>');
+});
 
 app.listen(4000, function (erro) {
-   if (erro) {
-       console.log('Ocorreu um erro ao iniciar o servidor! ')
-   }
+  if (erro) {
+    console.log('Ocorreu um erro ao iniciar o servidor! ');
+  }
 
-   console.log('Servidor iniciado!')
-})
+  console.log('Servidor iniciado!');
+});
